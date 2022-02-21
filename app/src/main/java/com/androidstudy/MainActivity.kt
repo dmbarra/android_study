517e682d7a7cc9185f7f0495b2af52769309abff
Daniel Machado Barra <daniel.barra@bancointer.com.br>
2022-02-21 16:00:16 -0300

    Create toolbar

--- app/src/main/java/com/androidstudy/MainActivity.kt
+++ app/src/main/java/com/androidstudy/MainActivity.kt
@@ -2,10 +2,23 @@ package com.androidstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val materialToolbar:Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(materialToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
