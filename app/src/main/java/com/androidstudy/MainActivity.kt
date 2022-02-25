6528204ad35fd56e63fb60bab02c174b5b069506
Daniel Machado Barra <daniel.barra@bancointer.com.br>
2022-02-25 10:05:25 -0300

    Add login and transaction for another fragment

--- app/src/main/java/com/androidstudy/MainActivity.kt
+++ app/src/main/java/com/androidstudy/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.androidstudy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import com.google.android.material.appbar.MaterialToolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.androidstudy.ui.login.LoginFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,11 +22,29 @@ class MainActivity : AppCompatActivity() {
 
         val materialToolbar:Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(materialToolbar)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_login_menu -> {
+                replaceFragmentOnFrame(LoginFragment())
+            }
+            else -> {}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragmentOnFrame(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_main_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
