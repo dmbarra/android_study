e5c8ffe787c6856ca5ed85f8aa41eb88d690ea4f
Daniel Machado Barra <daniel.barra@bancointer.com.br>
2022-08-31 14:57:45 -0300

    Expand Instrumented tests for login

--- app/src/androidTest/java/com/androidstudy/instrumented/LoginFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/com/androidstudy/instrumented/LoginFragmentInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.androidstudy.instrumented
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -9,6 +10,9 @@ import androidx.test.filters.MediumTest
 import com.androidstudy.R
 import com.androidstudy.ui.home.HomeFragment
 import com.androidstudy.ui.login.LoginFragment
+import okhttp3.internal.notify
+import org.hamcrest.core.IsNot.not
+import org.junit.Before
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,18 +21,32 @@ import org.junit.runner.RunWith
 @MediumTest
 class LoginFragmentInstrumentedTest {
 
-    @Test fun shouldLoadAndLoginFragment() {
+    @Before fun setUp() {
         val scenario = launchFragmentInContainer<LoginFragment>(
             themeResId = R.style.Theme_AppCompat
         )
+    }
 
+    @Test fun shouldLoadAndLoginFragment() {
         onView(withResourceName("username")).check(matches(isDisplayed()))
         onView(withResourceName("username")).check(matches(withHint(R.string.prompt_email)))
         onView(withResourceName("password")).check(matches(isDisplayed()))
         onView(withResourceName("password")).check(matches(withHint(R.string.prompt_password)))
         onView(withResourceName("login")).check(matches(isDisplayed()))
         onView(withResourceName("login")).check(matches(withText(R.string.action_sign_in)))
+        onView(withResourceName("login")).check(matches(not(isEnabled())))
     }
 
+    @Test fun shouldEnableButtonLogin() {
+        onView(withResourceName("username")).perform(typeText("username"))
+        onView(withResourceName("password")).perform(typeText("password"))
+        onView(withResourceName("login")).check(matches(isEnabled()))
+    }
+
+    @Test fun shouldShowInvalidPasswordSize() {
+        onView(withResourceName("username")).perform(typeText("username"))
+        onView(withResourceName("password")).perform(typeText("pas"))
+        onView(withResourceName("password")).check(matches(hasErrorText("Password must be >5 characters")))
+    }
 
 }
